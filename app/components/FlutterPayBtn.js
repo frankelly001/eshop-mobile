f5e8d731bf8e5a74e4528db9598dcb3b6f926a89
franklyn <frankelly344@gmail.com>
2022-06-15 01:14:20 +0100

    added flutterwave payment

---
+++ app/components/FlutterPayBtn.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import AppGradientBtn from './AppGradientBtn';
+import {PayWithFlutterwave} from 'flutterwave-react-native';
+import {formatToCurrency} from '../utilities/formatToCurr';
+import AppButton from './AppButton';
+
+const FlutterPayBtn = ({total, email}) => {
+  // length must be a num
+  const generateTransactionRef = length => {
+    var result = '';
+    var characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return `flw_tx_ref_${result}`;
+  };
+
+  console.log(generateTransactionRef(10));
+
+  const handleOnRedirect = data => {
+    console.log(data, "incase status it's successful, cancelled or failed");
+  };
+
+  // if (1) return null;
+  return (
+    <PayWithFlutterwave
+      onRedirect={handleOnRedirect}
+      options={{
+        tx_ref: generateTransactionRef(10),
+        // authorization: 'FLWPUBK_TEST-e1399dbd0b80e614e77eb9000e0ba5b2-X',
+        // tx_ref: Date.now(),
+        authorization: 'FLWPUBK_TEST-e1399dbd0b80e614e77eb9000e0ba5b2-X',
+        customer: {
+          email: email,
+        },
+        amount: total,
+        currency: 'NGN',
+        payment_options: 'card',
+      }}
+      customButton={({disabled, onPress, isInitializing}) => (
+        <AppGradientBtn
+          label={`PAY NOW: ${formatToCurrency(total)}`}
+          labelStyle={{fontWeight: '700'}}
+          onPress={onPress}
+          isBusy={isInitializing}
+          disabled={disabled}
+        />
+      )}
+    />
+  );
+};
+
+export default FlutterPayBtn;
